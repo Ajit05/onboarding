54791d95890f197a3fb09af3929c44bde3716229
Ajit05 <ajity050@gmail.com>
2017-03-08 00:41:05 +0530

    1

---
+++ onboarding/src/main/java/com/studio/onboarding/App.java
@@ -0,0 +1,13 @@
+package com.studio.onboarding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
